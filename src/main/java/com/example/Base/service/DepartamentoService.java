fa1bd20b2c5a8aa77b0b94ba540406df7606f700
unknown <marcosesteves2005@gmail.com>
2026-03-23 22:19:06 -0300

    Service

---
+++ src/main/java/com/example/Base/service/DepartamentoService.java
@@ -0,0 +1,32 @@
+package com.example.Base.service;
+
+import com.example.Base.entity.Departamento;
+import com.example.Base.repository.DepartamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DepartamentoService {
+
+    @Autowired
+    private DepartamentoRepository repository;
+
+    public Departamento salvar(Departamento departamento) {
+        return repository.save(departamento);
+    }
+
+    public List<Departamento> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Optional<Departamento> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+}
